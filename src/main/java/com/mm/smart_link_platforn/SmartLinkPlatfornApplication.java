4bb80daaabd04f46e269168443baf740816c3141
Marcelo Melo <marcelovmelo201@gmail.com>
2026-03-11 17:27:19 -0300

    Commit inicial

---
+++ src/main/java/com/mm/smart_link_platforn/SmartLinkPlatfornApplication.java
@@ -0,0 +1,13 @@
+package com.mm.smart_link_platforn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLinkPlatfornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLinkPlatfornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mm/smart_link_platforn/SmartLinkPlatfornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.smart_link_platforn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLinkPlatfornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
