973f1e6b03d2fe0284c8236ee80776df079d1658
Marcelo Melo <marcelovmelo201@gmail.com>
2026-03-13 18:23:40 -0300

    Alteração nos repositório

--- src/main/java/com/mm/smart_link_platform/repository/LinkRepository.java
+++ src/main/java/com/mm/smart_link_platform/repository/LinkRepository.java
@@ -1,12 +1,14 @@
 package com.mm.smart_link_platform.repository;
 
 import com.mm.smart_link_platform.entity.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface LinkRepository {
+public interface LinkRepository extends JpaRepository<Link, UUID> {
 
     Optional<Link> findByShortCode(String shortCode);
     boolean existsByShortCode(String shortCode);
