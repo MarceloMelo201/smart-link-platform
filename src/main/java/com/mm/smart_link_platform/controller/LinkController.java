06784fb5765f85ccb785060c3a7ccce400c17280
Marcelo Melo <marcelovmelo201@gmail.com>
2026-03-18 18:35:34 -0300

    Alteração no LinkController

--- src/main/java/com/mm/smart_link_platform/controller/LinkController.java
+++ src/main/java/com/mm/smart_link_platform/controller/LinkController.java
@@ -2,33 +2,37 @@ package com.mm.smart_link_platform.controller;
 
 import com.mm.smart_link_platform.dto.CreateLinkRequest;
 import com.mm.smart_link_platform.dto.CreateLinkResponse;
+import com.mm.smart_link_platform.dto.LinkStatsResponse;
+import com.mm.smart_link_platform.service.AnalyticsService;
 import com.mm.smart_link_platform.service.LinkService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/links")
 public class LinkController {
 
-    private final LinkService service;
+    private final LinkService linkService;
+    private final AnalyticsService analyticsService;
 
-    @Autowired
-    public LinkController(LinkService service) {
-        this.service = service;
+    public LinkController(LinkService linkService, AnalyticsService analyticsService) {
+        this.linkService = linkService;
+        this.analyticsService = analyticsService;
     }
 
     @PostMapping
-    ResponseEntity<CreateLinkResponse> createShortLink(
+    public ResponseEntity<CreateLinkResponse> createShortLink(
             @RequestBody @Valid CreateLinkRequest request
             ) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(service.createShortLink(request));
+                .body(linkService.createShortLink(request));
+    }
+
+    @GetMapping("/{shortCode}/stats")
+    public ResponseEntity<LinkStatsResponse> getLinkStats(@PathVariable String shortCode) {
+        return ResponseEntity.ok(analyticsService.getLinkStats(shortCode));
     }
 
 }
