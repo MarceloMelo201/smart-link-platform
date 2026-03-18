72252767529fef1efca647a661c83d165fd2373e
Marcelo Melo <marcelovmelo201@gmail.com>
2026-03-18 18:35:51 -0300

    Alteração no RedirectController

--- src/main/java/com/mm/smart_link_platform/controller/RedirectController.java
+++ src/main/java/com/mm/smart_link_platform/controller/RedirectController.java
@@ -1,28 +1,34 @@
 package com.mm.smart_link_platform.controller;
 
 import com.mm.smart_link_platform.service.RedirectService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
-@RequestMapping("/api")
 public class RedirectController {
 
-    private final RedirectService service;
+    private final RedirectService redirectService;
 
-    public RedirectController(RedirectService service) {
-        this.service = service;
+    public RedirectController(RedirectService redirectService) {
+        this.redirectService = redirectService;
     }
 
     @GetMapping("/{shortCode}")
-    public ResponseEntity<Void> resolveLink(@PathVariable String shortCode) {
-        String url = service.resolveLink(shortCode);
+    public ResponseEntity<Void> resolveLink(
+            @PathVariable String shortCode,
+            HttpServletRequest request) {
+
+        String ip = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+        String referer = request.getHeader("Referer");
+        String url = redirectService.resolveLink(shortCode, ip, userAgent, referer);
+
         return ResponseEntity
                 .status(HttpStatus.FOUND)
                 .location(URI.create(url))
