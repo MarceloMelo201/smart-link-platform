3e7fb25b5a0255e8138d7248c11e242e1ab91005
Marcelo Melo <marcelovmelo201@gmail.com>
2026-03-21 20:16:26 -0300

    Alterações no LinkStatsResponse

--- src/main/java/com/mm/smart_link_platform/dto/LinkStatsResponse.java
+++ src/main/java/com/mm/smart_link_platform/dto/LinkStatsResponse.java
@@ -3,15 +3,17 @@ package com.mm.smart_link_platform.dto;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 public record LinkStatsResponse(
 
         String originalUrl,
-        String shortUrl,
+        String shortCode,
         Long totalClicks,
         Long uniqueIps,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        List<AccessLogResponse> recentAccesses
 
 ) {
 }
