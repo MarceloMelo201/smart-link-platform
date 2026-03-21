f1b14ea430a2bf835f67ba3ec18a92dc3b67ed84
Marcelo Melo <marcelovmelo201@gmail.com>
2026-03-21 20:17:40 -0300

    Alterações no AccessRepository

--- src/main/java/com/mm/smart_link_platform/repository/AccessRepository.java
+++ src/main/java/com/mm/smart_link_platform/repository/AccessRepository.java
@@ -12,11 +12,11 @@ import java.util.UUID;
 @Repository
 public interface AccessRepository extends JpaRepository<AccessLog, UUID> {
 
-    List<AccessLog> findAllByLinkId(UUID linkId);
     Long countByLinkId(UUID linkId);
 
     @Query("SELECT COUNT(DISTINCT a.ip) FROM access a WHERE a.linkId = :linkId")
     Long countUniqueVisitors(@Param("linkId") UUID linkId);
 
     List<AccessLog> findTop10ByLinkIdOrderByAccessTimeDesc(UUID linkId);
+
 }
