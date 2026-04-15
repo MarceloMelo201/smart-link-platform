2c7a38c2cf69a1fb4a42600dc5131ddead3c713d
Marcelo Melo <marcelovmelo201@gmail.com>
2026-04-15 19:39:33 -0300

    Alteração na exceção personalizada LinkExpiredException

--- src/main/java/com/mm/smart_link_platform/exception/LinkExpiredException.java
+++ src/main/java/com/mm/smart_link_platform/exception/LinkExpiredException.java
@@ -2,6 +2,6 @@ package com.mm.smart_link_platform.exception;
 
 public class LinkExpiredException extends RuntimeException{
     public LinkExpiredException() {
-        super("Link expired, please try again");
+        super("Link expired, please try again.");
     }
 }
